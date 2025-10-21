e338bf546a723d6d6c6ff72365240b9f4956c579
dheim123 <dheim@midland.k12.ia.us>
2025-10-21 11:43:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var addition = 50;
+addition += 5;
+
+var subtraction = 40;
+subtraction -= 1;
+
+var multiplication = 5;
+multiplication *= 7;
+
+var division = 6;
+division /= 2;
+
+document.getElementById("result").innerHTML = multiplication;
+
 
